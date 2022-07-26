ff1309132246a8faf38d92b2833c48818e288655
tanhoai11 <109981247+tanhoai11@users.noreply.github.com>
2022-07-26 11:41:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Ghét anh nhắm lun :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì thương <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối thứ 7 6h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
